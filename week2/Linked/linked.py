2e1f638ef57045997eae4039cab55c2d0335b25d
Christian Van Loon <66512995+WarMachine-282@users.noreply.github.com>
2021-09-17 22:43:56 -0700

    finished week 2

---
+++ week2/Linked/linked.py
@@ -0,0 +1,39 @@
+class Node:
+    # the singly linked node 
+    def __init__(self, data = None):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        # where the list is created 
+        self.count = 0
+        self.head = None
+        self.tail = None
+    def iteration(self):
+        # where the iteration in the linked list takes place
+        current_item = self.head
+        while current_item:
+            value =current_item.next
+            yield value
+    def AppendItems(self, data):
+        # appending items to list 
+        node = Node(data)
+        if self.tail:
+            self.tail.next = node
+            self.tail = node
+        else: 
+            self.head = node
+            self.tail = node
+        self.count += 1
+
+items = LinkedList()
+items.AppendItems('PHP')
+items.AppendItems('Python')
+items.AppendItems('C#')
+items.AppendItems('C++')
+items.AppendItems('Java')
+for value in items.iteration():
+    print(value)
+    print("head.data", items.head.data)
+    print("tail.data", items.tail.data)
