223c9d9d6f308bee9d51ac6bb2f9c31bf763635a
Christian Van Loon <66512995+WarMachine-282@users.noreply.github.com>
2021-09-26 22:15:51 -0700

    week4

---
+++ week4/Hashing/hashing.py
@@ -0,0 +1,13 @@
+# initializing objects
+
+int_val = hash(4)
+# integer value^ 
+flt_val = hash(24.56)
+# float value ^ 
+str_val = hash("PythonIsBest")
+# string value^
+
+# printing the object values defined up top
+print(f"For integer value, Hash : {int_val}")
+print(f"For float value, Hash: {flt_val}")
+print(f"For string value and {'PythonIsBest'}, Hash: {str_val}")
